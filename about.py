ed666017c4c5c52700a361562d14058618aa1c92
Mohammad-Moiz <127727314+Mohammad-Moiz@users.noreply.github.com>
2024-06-13 03:46:07 +0500

    Add files via upload

---
+++ about.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.subheader('MovieMate is a Website Created for Users to...')
+    st.subheader('"Watch the Movies based on the Recommendations and Reviews provided by the Users."')
+    st.markdown('Created by: [Muhammad Moiz](https://github.com/Mohammad-Moiz)')
+    st.markdown('Contact via mail: [kmohammadmoiz01@gmail.com]')
